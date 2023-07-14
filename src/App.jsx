a1a8fcf3f7d2a06b2103742c771bad61ffd97147
Panji Ramadanu <36503158+pnjrmdn@users.noreply.github.com>
2023-07-14 23:21:36 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,48 +1,44 @@
 
 import React from "react";
 
-// component create by class
-class Button extends React.Component{
-  render() {
-    return(
-    <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
-       Buy now
-    </button>
-    )
-  };
-}
+// // component create by class
+// class Button extends React.Component{
+//   render() {
+//     return(
+//     <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+//        Buy now
+//     </button>
+//     )
+//   };
+// }
 
-// component create by function
-function ButtonBlack(){
-  return(
-    <button className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white" type="submit">
-       Buy now
-    </button>
-    );
-}
+// // component create by function
+// function ButtonBlack(){
+//   return(
+//     <button className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white" type="submit">
+//        Buy now
+//     </button>
+//     );
+// }
 
-const ButtonRed = () => {
-  return(
-    <button className="h-10 px-6 font-semibold rounded-md bg-red-700 text-white" type="submit">
-       Buy now
+const Button = (props) => {
+  // distructturing consep
+  const { children, variant} = props;
+  return (
+    <button className={`h-10 px-6 font-semibold rounded-md ${ßvariant} text-white" type="submit`}>
+    {children}
     </button>
-    );
+  );
 }
 
 function App() {
   return (
-    <div className='flex justify-center bg-blue-600 min-h-screen items-center'>
-      
+    <div className='flex justify-center bg-slate-600 min-h-screen items-center'>
+
       <div className="flex gap-x-5">
-      <Button></Button>
-      <Button></Button>
-      <Button></Button>
-
-      <p>this is button black</p>
-      <ButtonBlack></ButtonBlack>
-
-      <p>this is button red</p>
-      <ButtonRed></ButtonRed>
+        <p>this is button red</p>
+        <Button variant="bg-blue-500"> Sign in</Button>
+        <Button variant="bg-green-500"> Sign up</Button>
       </div>
 
     </div>)
