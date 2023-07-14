a2f1c0656f29a292ccb14f7dabb5d2fd7fba3fe6
Panji Ramadanu <36503158+pnjrmdn@users.noreply.github.com>
2023-07-14 22:52:46 +0800

    Initial commit

---
+++ doc.sh
@@ -0,0 +1,12 @@
+//install react vai vite
+npm create vite@latest {name_project} -- --template react
+
+//start react web
+npm run dev
+
+// install tailwind
+npm install -D tailwindcss postcss autoprefixer
+npx tailwindcss init -p
+
+//following step on webiste tailwind
+
---
+++ src/App.jsx
@@ -0,0 +1,48 @@
+
+import React from "react";
+
+// component create by class
+class Button extends React.Component{
+  render() {
+    return(
+    <button className="h-10 px-6 font-semibold rounded-md bg-black text-white" type="submit">
+       Buy now
+    </button>
+    )
+  };
+}
+
+// component create by function
+function ButtonBlack(){
+  return(
+    <button className="h-10 px-6 font-semibold rounded-md bg-slate-700 text-white" type="submit">
+       Buy now
+    </button>
+    );
+}
+
+const ButtonRed = () => {
+  return(
+    <button className="h-10 px-6 font-semibold rounded-md bg-red-700 text-white" type="submit">
+       Buy now
+    </button>
+    );
+}
+
+function App() {
+  return (
+    <div className='flex justify-center bg-blue-600 min-h-screen items-center'>
+      <Button></Button>
+      <Button></Button>
+      <Button></Button>
+
+      <p>this is button black</p>
+      <ButtonBlack></ButtonBlack>
+
+      <p>this is button red</p>
+      <ButtonRed></ButtonRed>
+
+    </div>)
+}
+
+export default App;
